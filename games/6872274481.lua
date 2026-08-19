7a6f3ba2b9c7bbed671f19dfa15395ee4531bd62
9wz13 <yoitsjaystar@gmail.com>
2026-08-19 19:27:17 -0400

    Adjust chargedAttack parameters for Killaura

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -2273,8 +2273,8 @@ run(function()
 										-- Safe Killaura helpers: throttles/smooths attacks to avoid server ratelimit
 								AttackRemote:FireServer({
 											weapon = sword.tool,
-											chargedAttack = {chargeRatio = 1},
-											lastSwingServerTimeDelta = 1,
+											chargedAttack = {chargeRatio = 1200},
+											lastSwingServerTimeDelta = 12000,
 											entityInstance = v.Character,
 											validate = {
 												raycast = {
